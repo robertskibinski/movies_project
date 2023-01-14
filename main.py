6a1cbada1e72b496818a807ab5d1cd55843f32e8
rober <robert.skibinski@poczta.onet.pl>
2023-01-14 14:52:53 +0100

    added request to api. Problem with inheritance in HTML files

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from flask import Flask, render_template
+import requests
+import random
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def homepage():
-    movies = []
-    return render_template("index.html", movies=movies)
+    response = requests.get("https://api.themoviedb.org/3/movie/popular?api_key=34b3fbd4dbabd957d6d90c188a2273ef")
+    movies = response.json()
+    ids = []
+    titles = []
+    posters = []
+    for i in range(8):
+        randomNumber = random.randint(0, 19)
+        while movies['results'][randomNumber]['id'] in ids:
+            randomNumber = random.randint(0, 19)
+        ids.append(movies['results'][randomNumber]['id'])
+        titles.append(movies['results'][randomNumber]['title'])
+        posters.append(movies['results'][randomNumber]['poster_path'])
+    return render_template("index.html", ids = ids, titles = titles, posters = posters)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
