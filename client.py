7a5c01d077f87928d43fcd13df11fe08d22eaf15
rober <robert.skibinski@poczta.onet.pl>
2023-01-18 17:15:35 +0100

    corrects f-strings

--- client.py
+++ client.py
@@ -20,25 +20,22 @@ def get_movies(list_type):
     movies = []
     ids = []
     for i in range(8):
-        randomNumber = random.randint(0, 19)
-        while randomNumber in ids:
-            randomNumber = random.randint(0, 19)
-        ids.append(randomNumber)
-        data = json_response['results'][randomNumber]
-        movie = Movie(id=data["id"],title=data["title"], poster=data["poster_path"])
+        random_number = random.randint(0, 19)
+        while random_number in ids:
+            random_number = random.randint(0, 19)
+        ids.append(random_number)
+        data = json_response['results'][random_number]
+        movie = Movie(id=data["id"], title=data["title"], poster=data["poster_path"])
         movies.append(movie)
-        # movies.append(Movie(json_response['results'][randomNumber]['id'],
-        #                     json_response['results'][randomNumber]['title'],
-        #                     json_response['results'][randomNumber]['poster_path']))
     return movies
 
 
 def get_poster_url(path, size):
-    return "https://image.tmdb.org/t/p/" + size + path
+    return f"https://image.tmdb.org/t/p/{size}{ path}"
 
 
 def get_movie(id):
-    response = requests.get("https://api.themoviedb.org/3/movie/" + id + "?api_key=34b3fbd4dbabd957d6d90c188a2273ef")
+    response = requests.get(f"https://api.themoviedb.org/3/movie/{id}?api_key=34b3fbd4dbabd957d6d90c188a2273ef")
     json_response = response.json()
     genres = [entry["name"] for entry in json_response["genres"]]
     movie = Movie(
@@ -54,13 +51,12 @@ def get_movie(id):
 
 
 def get_cast(id):
-    response = requests.get(
-        "https://api.themoviedb.org/3/movie/" + id + "/credits?api_key=34b3fbd4dbabd957d6d90c188a2273ef")
+    response = requests.get(f"https://api.themoviedb.org/3/movie/{id}/credits?api_key=34b3fbd4dbabd957d6d90c188a2273ef")
     json_response = response.json()
     cast = json_response["cast"]
     cast_with_profile_path = list(filter(lambda cast: cast["profile_path"] is not None, cast))
     casts = []
     for entry in cast_with_profile_path[:4]:
-        cast = Cast(id=entry["id"],name=entry["name"], profile=entry["profile_path"])
+        cast = Cast(id=entry["id"], name=entry["name"], profile=entry["profile_path"])
         casts.append(cast)
     return casts
